5e40db85095888e7ccb4eff112a5461b1f7a5d3a
J. Eduardo Montoya S <33884031+Jemscabo98@users.noreply.github.com>
2021-02-28 12:20:48 -0700

    Project Skeleton

---
+++ src/person/person.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Eduardo Montoya
+ */
+public class person {
+    public String name;
+    public double height;
+    public double weight;
+    public double bmi;
+    public String meaning;
+
+    public person(String name, double height, double weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
